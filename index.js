a35ae2b5d3703ae401bc2f9107faaeb5ef20290c
Ron <ronmycode@gmail.com>
2021-08-21 15:06:36 -0500

    add boostrap

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-// app.set('views', path.join(__dirname,"views"))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname,"views"))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res)=>{
