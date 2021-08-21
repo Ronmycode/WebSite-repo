f3f2c89d74b881af850c268cfab541b4bda10ddf
Ron <ronmycode@gmail.com>
2021-08-21 17:45:15 -0500

    adding css file to home page

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname,"views"))
+app.set('views', path.join(__dirname,'views'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res)=>{
