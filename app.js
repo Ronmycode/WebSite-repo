69895ec4db07e7fd3129b1ffb08070f059e20d5c
Ron <ronmycode@gmail.com>
2021-09-02 14:19:02 -0500

    add scss files and conection to html files, also modify package-jason commands

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+
+//middleware
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname,'views'))
 app.set('view engine', 'ejs')
 
+
+
+//routes
 app.get('/', (req, res)=>{
     res.render('home.ejs')
 })
